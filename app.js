6fe876abbb1f4edd1b34aef310d69b9c7c4d8d05
omana <omanandswami2005@gmail.com>
2024-03-31 22:15:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+require('dotenv').config();
+const path = require('path');
+
+const connectToMongoDB = require('./mongoConnection');
+
+
+const classRoutes = require('./routes/classRoutes'); // Import the router module for classes
+const facultyRoutes = require('./routes/facultyRoutes');
+const studentRoutes = require('./routes/studentRoutes'); // Import the router module for students
+const timeSlotRoutes = require('./routes/timeSlotRoutes');
+const attendanceRoutes = require('./routes/attendanceRoutes');
+
+const authRoutes = require('./routes/authRoutes'); // Adjust the path based on your project structure
+
+
+const app = express();
+///////////////////////////////////////////////////////////////////
+connectToMongoDB();
+///////////////////////////////////////////////////////////////////
+const PORT = process.env.PORT || 5000;
+///////////////////////////////////////////////////////////////////
+
+// Middleware to parse JSON, URL-encoded data and cookies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+////////////////////////////////////////////////////////////////////
+app.use('/class', classRoutes);
+app.use('/student', studentRoutes);
+app.use('/faculty', facultyRoutes);
+app.use('/timeSlot', timeSlotRoutes);
+app.use('/attendance', attendanceRoutes);
+
+app.use('/auth', authRoutes);
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ mongoConnection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+
+const connectToMongoDB = async () => {
+    mongoose
+  .connect('mongodb+srv://om:omiii@atlascluster.zo09joq.mongodb.net/VPolyServer?retryWrites=true&w=majority')
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error(err));
+}
+
+module.exports = connectToMongoDB;
